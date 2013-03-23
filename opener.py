041d19b79b0478c2a5f8a50519034e4807181d66
yunxing <yunxing@upthere.com>
2013-03-23 02:05:25 -0400

    added the grainaulity of buffer

--- opener.py
+++ opener.py
@@ -19,8 +19,9 @@ class Opener(object):
         logging.info("started downloading %s " % url)
         f = self.opener.open(url)
         total = int(f.info().getheader("Content-Length", "0"))
-        BUFFER_SIZE = total / 10
-        if BUFFER_SIZE == 0: BUFFER_SIZE = 1024
+        BUFFER_SIZE = total / 100
+        if BUFFER_SIZE == 0:
+            BUFFER_SIZE = 1024
         downloaded = 0
         with open(path, "wb", 0) as out:
             for data in iter((lambda:f.read(BUFFER_SIZE)),''):
